879db53480aae939ae6f37be5fb878d981c0c4ca
DreamChaser55 <otutko@gmail.com>
2026-07-07 17:35:25 +0200

    Updated the logging configuration in `game.py` to clear the log file each time the game is launched.

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
     handlers=[
-        logging.FileHandler("game.log"),
+        logging.FileHandler("game.log", mode='w'),
         logging.StreamHandler()
     ]
 )
