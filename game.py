a53ec2aecc4fbc52d7c7f5bb49359b26b2bf5253
DreamChaser55 <otutko@gmail.com>
2026-07-25 15:40:45 +0200

    logging: include date only in initial log entry timestamp
    Introduce `GameLogFormatter` to format the date on the first initialization log line while omitting it from subsequent log line timestamps.

--- game.py
+++ game.py
@@ -1,5 +1,19 @@
 import logging
 
+class GameLogFormatter(logging.Formatter):
+    """Custom logging formatter that includes the date only on the first logged line."""
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+        self.first_record = True
+
+    def formatTime(self, record, datefmt=None):
+        if self.first_record:
+            self.first_record = False
+            self.default_time_format = '%Y-%m-%d %H:%M:%S'
+        else:
+            self.default_time_format = '%H:%M:%S'
+        return super().formatTime(record, datefmt)
+
 def setup_logging(log_to_file: bool = False):
     """Configure logging for the application.
 
@@ -9,18 +23,19 @@ def setup_logging(log_to_file: bool = False):
     """
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 
     for handler in root_logger.handlers[:]:
         root_logger.removeHandler(handler)
 
+    fmt = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+    stream_handler.setFormatter(GameLogFormatter(fmt))
     root_logger.addHandler(stream_handler)
 
     if log_to_file:
         file_handler = logging.FileHandler("game.log", mode='w')
-        file_handler.setFormatter(formatter)
+        file_handler.setFormatter(GameLogFormatter(fmt))
         root_logger.addHandler(file_handler)
 
 setup_logging(log_to_file=False)
